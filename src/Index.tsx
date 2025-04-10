b61bb31bf91428dfba7431f6dd795ec38805a20a
Sai-Kit Hui <saikit@areyoudesign.com>
2025-04-10 16:55:51 -0400

    fix route in landing page

--- src/Index.tsx
+++ src/Index.tsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router'
 function Index() {
   return (
     <div className='p-4'>
-        <h1 className='font-bold text-2xl mb-4'>Welcome to Async Cook, an App for mobile devices and tablets that display easy-to-follow recipes for home cooks. App and recipes developed by <Link className='underline' to="http://saikithui.com">Sai-Kit Hui</Link></h1>
+        <h1 className='font-bold text-2xl mb-4'>Welcome to Async Cook, an App for mobile devices and tablets that display easy-to-follow recipes for home cooks. App and recipes developed by <Link className='underline text-blue-500' to="http://saikithui.com">Sai-Kit Hui</Link></h1>
         <h2 className='text-xl mb-2'>Recipe List</h2>
-        <ul>
+        <ul className='list-disc list-outside pl-4'>
           <li><Link to="pasta" className='underline'>Roasted Tomato and Garlic Pasta</Link></li>
-          <li><Link to="lestture" className='underline'>Boiled Lettuce in Oyster and Garlic Sauce</Link></li>
+          <li><Link to="lettuce" className='underline'>Boiled Lettuce in Oyster and Garlic Sauce</Link></li>
         </ul>
     </div>
   )
