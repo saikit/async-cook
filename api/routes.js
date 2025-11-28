31f6b79420d034746626c043ac4df0dfc06be968
Sai-Kit Hui <saikit@areyoudesign.com>
2025-11-28 14:43:27 -0500

    remove outdated node server

--- api/routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getAllRecipes, getRecipeBySlug } from "./handlers.js";
-
-const appRouter = Router();
-
-appRouter.get("/", getAllRecipes);
-appRouter.get("/:slug", getRecipeBySlug);
-
-export default appRouter;
