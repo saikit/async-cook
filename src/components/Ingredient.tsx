664b70d891c1d87503e52e402aa0ff527a3bfac8
Sai-Kit Hui <saikit@areyoudesign.com>
2025-12-05 16:23:14 -0500

    backup db and add markdown formatting to ingredients

--- src/components/Ingredient.tsx
+++ src/components/Ingredient.tsx
@@ -1,25 +1,39 @@
 import { IngredientType } from "@/context/RecipeProvider"
 import RecipeNoteIcon from "./RecipeNoteIcon"
+import Markdown from "react-markdown"
 
 
 const Ingredient = ({ description, status }: { description: IngredientType['description'][0], status: IngredientType['status'] }) => {
   const { name, context, cooked } = description
-  const icon = (
-    (context) ?
-      (context.map((note, key) => <RecipeNoteIcon note={note} key={key}/>)) 
-    : null
+  const icons = (
+    context && context.map((note, key) => <RecipeNoteIcon note={note} key={key}/>) 
   )
+
+  const createMarkdownComponents = (includeIcons: boolean = false) => ({
+    p(props: React.HTMLProps<HTMLParagraphElement>) {
+      const {children, ...rest} = props;
+      return <p className="mb-0.5" {...rest}>{children}{includeIcons && icons}</p>;
+    },
+    strong(props: React.HTMLProps<HTMLElement>) {
+      const {children, ...rest} = props;
+      return <strong className="font-bold" {...rest}>{children}</strong>;
+    }
+  })
+
+  const markdownComponents = createMarkdownComponents(true)
+  const markdownComponentsWithoutIcons = createMarkdownComponents(false)
+
   const content = (
     (status === "active" && cooked ) ?
-      <li><b className="text-stone-500">{name}</b>{icon}</li>
+      <li><b className="text-stone-500"><Markdown components={markdownComponents}>{name}</Markdown></b></li>
     :
     (status === "active") ?
-      <li className={status}><b>{name}</b>{icon}</li>
+      <li className={status}><b><Markdown components={markdownComponents}>{name}</Markdown></b></li>
     :
     (status === "complete") ?
-      <li className={status}><s>{name}</s></li>
+      <li className={status}><s><Markdown components={markdownComponentsWithoutIcons}>{name}</Markdown></s></li>
     :
-      <li className={status}>{name}{icon}</li>
+      <li className={status}><Markdown components={markdownComponents}>{name}</Markdown></li>
   )
 
   return content
