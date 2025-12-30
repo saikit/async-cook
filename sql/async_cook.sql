8531326bef97ffa8c453a0cdc34fc79c8739f982
Sai-Kit Hui <saikit@areyoudesign.com>
2025-12-30 11:34:05 -0500

    backup sql db

--- sql/async_cook.sql
+++ sql/async_cook.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mysql
--- Generation Time: Dec 22, 2025 at 03:09 AM
+-- Generation Time: Dec 30, 2025 at 06:12 AM
 -- Server version: 8.0.32
 -- PHP Version: 8.3.27
 
@@ -41,7 +41,8 @@ CREATE TABLE `calculator` (
 
 INSERT INTO `calculator` (`id`, `recipe_id`, `ingredient_group_id`, `text`) VALUES
 (1, 3, 20, 'For each gram of mung beans, add **2ml** of water. For each gram of rice, add **1ml** of water. \r\n\r\n`(mung beans x 2) + rice = water + soy sauce`\r\n\r\nAdjust soy sauce amount to desired taste'),
-(2, 4, 15, 'For each gram of beans, add **4ml** of water. Ensure that beans are submerged throughout cooking process.');
+(2, 4, 15, 'For each gram of beans, add **4ml** of water. Ensure that beans are submerged throughout cooking process.'),
+(3, 5, 33, 'For each gram of lentils, add **2ml** of water. For each gram of rice, add **1ml** of water. \r\n\r\n`(lentils x 2) + rice = water + salt`\r\n\r\nAdjust salt amount to desired taste');
 
 -- --------------------------------------------------------
 
@@ -85,7 +86,11 @@ INSERT INTO `context` (`id`, `recipe_id`, `ingredient_id`, `instruction_id`, `ca
 (19, 3, 77, NULL, 'recommendation', 'Other cured or preserved meats such as bacon works as well.'),
 (20, 3, 79, NULL, 'recommendation', 'Any flavored oils and fats will work.'),
 (21, 1, 12, NULL, 'recommendation', 'Use just enough to season pasta if you plan to use the pasta water.'),
-(22, 1, 14, NULL, 'recommendation', 'If using fresh basil, chop into smaller pieces.');
+(22, 1, 14, NULL, 'recommendation', 'If using fresh basil, chop into smaller pieces.'),
+(23, 6, NULL, 47, 'recommendation', 'Separate the leaves and the stem. Stem can be cut further to smaller pieces'),
+(30, 5, 149, NULL, 'recommendation', 'Basmati is traditionally used, but any type of rice will work'),
+(31, 5, 154, NULL, 'alert', 'Because the recipe involves making crispy onions and caramelized onions simultaneously, feel free to skip one of them'),
+(32, 5, NULL, 57, 'Explanation', 'Add just enough water to cook through the onions before it evaporates');
 
 -- --------------------------------------------------------
 
@@ -135,7 +140,7 @@ INSERT INTO `ingredients` (`id`, `recipe_id`, `quantity`, `unit`, `variable`, `n
 (20, 1, NULL, NULL, NULL, 'extra virgin olive oil', NULL, 0, NULL, 6, 7),
 (21, 1, NULL, NULL, NULL, 'salt or fish sauce', NULL, 0, NULL, 6, 8),
 (34, 1, NULL, NULL, NULL, 'cooked shrimp', NULL, 1, 'Shrimp', 6, 9),
-(35, 1, NULL, NULL, NULL, 'extra grated Parmesan cheese *(for serving)*', NULL, 0, NULL, 7, 1),
+(35, 1, NULL, NULL, NULL, 'grated Parmesan cheese *(for serving)*', NULL, 0, NULL, 7, 1),
 (36, 1, NULL, NULL, NULL, 'sliced mozzarella', NULL, 1, 'Mozzarella', 7, 2),
 (37, 2, 200, 'g', NULL, 'lettuce', 169247, 0, NULL, 1, 1),
 (38, 2, 1, NULL, NULL, 'scallion', NULL, 0, NULL, 1, 2),
@@ -178,7 +183,46 @@ INSERT INTO `ingredients` (`id`, `recipe_id`, `quantity`, `unit`, `variable`, `n
 (81, 4, 5, 'g', -1, 'salt', NULL, 0, NULL, 1, 3),
 (82, 4, 1, 'g', -1, 'baking soda *(optional)*', NULL, 0, NULL, 1, 4),
 (83, 3, NULL, NULL, NULL, 'cooked rice and beans', NULL, 1, NULL, 5, 1),
-(84, 4, NULL, NULL, NULL, 'roasted sweet potatoes', NULL, 1, NULL, 4, 2);
+(84, 4, NULL, NULL, NULL, 'roasted sweet potatoes', NULL, 1, NULL, 4, 2),
+(90, 6, 280, 'g', NULL, 'chicken gizzards', 171456, 0, NULL, 2, 1),
+(91, 6, 250, 'ml', NULL, 'dashi or chicken stock', NULL, 0, NULL, 3, 1),
+(92, 6, 15, 'g', NULL, 'miso paste', 172442, 0, NULL, 7, 1),
+(93, 6, 150, 'g', NULL, 'carrots', 2258586, 0, NULL, 1, 1),
+(94, 6, 150, 'g', NULL, 'daikon radish', 168451, 0, NULL, 1, 1),
+(127, 6, 15, 'g', NULL, 'gochujang', NULL, 0, NULL, 4, 1),
+(128, 6, 15, 'ml', NULL, 'soy sauce', NULL, 0, NULL, 4, 2),
+(129, 6, 15, 'ml', NULL, 'mirin', NULL, 0, NULL, 4, 3),
+(130, 6, 3, 'g', NULL, 'sugar', NULL, 0, NULL, 4, 4),
+(131, 6, NULL, NULL, NULL, 'neutral cooking oil', NULL, 0, NULL, 8, 1),
+(132, 6, 3, '', NULL, 'cloves of garlic', NULL, 0, NULL, 6, 1),
+(133, 6, 10, 'g', NULL, 'ginger', NULL, 0, NULL, 6, 2),
+(134, 6, 200, 'g', NULL, 'bok choy, any variety', 2685572, 0, NULL, 5, 1),
+(142, 6, NULL, NULL, NULL, 'chopped daikon', NULL, 1, NULL, 3, 2),
+(143, 6, NULL, NULL, NULL, 'chopped carrots', NULL, 1, NULL, 3, 3),
+(144, 6, NULL, NULL, NULL, 'chopped gizzards', NULL, 1, NULL, 3, 1),
+(145, 6, NULL, NULL, NULL, 'cooked gizzards', NULL, 1, NULL, 8, 2),
+(146, 6, NULL, NULL, NULL, 'minced garlic and ginger', NULL, 1, NULL, 8, 3),
+(147, 6, NULL, NULL, NULL, 'chopped bok choy', NULL, 1, NULL, 8, 4),
+(148, 6, NULL, NULL, NULL, 'stir fry sauce', NULL, 1, NULL, 8, 5),
+(149, 5, 150, 'g', 1, 'rice', 2512381, 0, NULL, 1, 1),
+(150, 5, 150, 'g', 2, 'brown or green lentils', 2644283, 0, NULL, 1, 2),
+(151, 5, 448, 'g', 0, 'water', NULL, 0, NULL, 1, 3),
+(152, 5, 2, 'g', -1, 'salt', NULL, 0, NULL, 1, 4),
+(154, 5, 2, NULL, NULL, 'onions', 790646, 0, NULL, 2, 1),
+(155, 5, 1, NULL, NULL, 'scallion *(optional)*', NULL, 0, NULL, 2, 2),
+(156, 5, 2, 'g', NULL, 'cumin seeds', NULL, 0, NULL, 4, 1),
+(157, 5, NULL, '', NULL, 'extra virgin olive oil', NULL, 0, NULL, 3, 3),
+(158, 5, NULL, NULL, NULL, 'salt', NULL, 0, NULL, 3, 4),
+(159, 5, NULL, NULL, NULL, 'water', NULL, 0, NULL, 3, 5),
+(161, 5, NULL, NULL, NULL, 'caramelized onions', NULL, 1, NULL, 4, 3),
+(162, 5, NULL, NULL, NULL, 'rice and lentils', NULL, 1, NULL, 4, 4),
+(163, 5, NULL, NULL, NULL, 'onion-infused oil', NULL, 1, NULL, 4, 5),
+(164, 5, NULL, NULL, NULL, 'sliced scallions', NULL, 1, NULL, 4, 2),
+(165, 5, NULL, NULL, NULL, 'sliced onions', NULL, 1, NULL, 3, 1),
+(166, 5, NULL, NULL, NULL, 'diced onions', NULL, 1, NULL, 3, 2),
+(167, 2, NULL, NULL, NULL, 'combined sauce', NULL, 1, NULL, 5, 5),
+(168, 2, NULL, NULL, NULL, 'cornstarch slurry', NULL, 1, NULL, 5, 6),
+(169, 1, NULL, NULL, NULL, 'extra virgin olive oil *(for serving)*', NULL, 0, NULL, 7, 2);
 
 -- --------------------------------------------------------
 
@@ -223,7 +267,19 @@ INSERT INTO `ingredient_groups` (`id`, `recipe_id`, `step`, `text`, `optional`) 
 (21, 3, 2, NULL, 'Mushrooms'),
 (22, 3, 3, NULL, 'Chinese sausage'),
 (23, 3, 4, NULL, NULL),
-(24, 3, 5, NULL, NULL);
+(24, 3, 5, NULL, NULL),
+(25, 6, 4, 'stir fry sauce', NULL),
+(26, 6, 1, NULL, NULL),
+(27, 6, 2, NULL, NULL),
+(28, 6, 3, NULL, NULL),
+(29, 6, 5, NULL, NULL),
+(30, 6, 6, NULL, NULL),
+(31, 6, 7, NULL, NULL),
+(32, 6, 8, NULL, NULL),
+(33, 5, 1, 'rice and lentils', NULL),
+(34, 5, 2, NULL, NULL),
+(35, 5, 3, NULL, NULL),
+(36, 5, 4, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -254,7 +310,7 @@ INSERT INTO `instructions` (`id`, `recipe_id`, `step`, `title`, `text`, `backgro
 (4, 1, 4, 'Boil pasta', 'Boil **pasta** in **salted** **water** for **1-2 minutes** before instructions for al dente. Set aside pasta and pasta cooking water.', 'Tomatoes and garlic are in the oven.', NULL, 1),
 (5, 1, 5, 'Pan-fry shrimp', 'Fry **seasoned shrimp** in a pan in high heat for **1 minute** until slightly brown. Remove from heat.', NULL, 'Shrimp', 1),
 (6, 1, 6, 'Stir-fry pasta', 'Remove peel from garlic. Pour roasted tomatoes and garlic into pan. Add **basil** and **red pepper flakes** to sauce. Add **cooked pasta** and some of the **cooked pasta water** to pan and stir on high heat.', NULL, NULL, 1),
-(7, 1, 7, 'Plate the pasta', 'Serve pasta with extra **grated Parmesan cheese** and **extra virgin olive oil**.', NULL, NULL, 1),
+(7, 1, 7, 'Plate the pasta', 'Serve pasta with extra **grated Parmesan cheese** and **olive oil**.', NULL, NULL, 1),
 (8, 1, 6, '', 'Remove pan from heat and add most or all of the **grated Parmesan cheese**.', NULL, NULL, 2),
 (9, 1, 6, '', 'Return pan to heat and add **black pepper** and **olive oil** and stir for another **30 seconds**.', NULL, NULL, 3),
 (10, 1, 6, '', 'Add **salt** to taste. Add more pasta water to adjust consistency.', NULL, NULL, 4),
@@ -262,7 +318,7 @@ INSERT INTO `instructions` (`id`, `recipe_id`, `step`, `title`, `text`, `backgro
 (13, 1, 7, 'Top with mozzarella', 'Top pasta with **sliced mozzarella**.', NULL, 'Mozzarella', 1),
 (14, 2, 1, 'Cut lettuce & aromatics', 'Wash and chop **lettuce** into bite sized pieces. Wash and chop **scallions** to rounds. Peel and mince **garlic cloves**.', NULL, NULL, 1),
 (15, 2, 2, 'Make the sauce', 'Combine **oyster sauce**, **soy sauce**, **dark soy sauce**, **canned tuna**, and **sugar**, and **MSG** in a bowl. Mix well. ', NULL, NULL, 1),
-(16, 2, 4, 'Boil water', 'Heat a pot of **salted water** to a boil.', NULL, NULL, 1),
+(16, 2, 4, 'Boil water', 'Bring the **water** to a boil and add **salt**.', NULL, NULL, 1),
 (17, 2, 5, 'Cook sauce', 'In a pan, heat up **neutral oil** over high heat until slightly smoking. Add **minced garlic**, **chopped scallion**, and **red pepper flakes** and fry until aromatic.', 'Water is boiling.', NULL, 1),
 (18, 2, 5, '', 'Add **combined sauce** to pan. Cook briefly then set flame to low and add **cornstarch slurry**. Cook until sauce is thickened.', NULL, NULL, 2),
 (19, 2, 6, 'Boil the lettuce', 'Add **chopped lettuce** to the salted boiling water and cook until leaves turn bright green. Remove lettuce from pan and drain well.', NULL, NULL, 1),
@@ -277,7 +333,26 @@ INSERT INTO `instructions` (`id`, `recipe_id`, `step`, `title`, `text`, `backgro
 (28, 3, 2, 'Prep the mushrooms', 'Wash **mushrooms** and dice into small pieces. Add to **mung bean and rice** mixture.\r\n', '', 'Mushrooms', 1),
 (29, 3, 3, 'Prep the cured meats', 'Dice **Chinese sausage** into small pieces. Add to **mung bean and rice** mixture.\r\n', NULL, 'Chinese sausage', 1),
 (30, 3, 4, 'Cook mung beans and rice', 'Cook **mixture** in electric pressure cooker for 15 minutes on high pressure or normal setting in a rice cooker. Natural release if using pressure cooker.', NULL, NULL, 1),
-(31, 3, 5, 'Plate dish', 'Mix **cooked rice and beans** with **lard or butter** if using. Add more salt or soy sauce to desired taste.', NULL, '', 1);
+(31, 3, 5, 'Plate dish', 'Mix **cooked rice and beans** with **lard or butter** if using. Add more salt or soy sauce to desired taste.', NULL, '', 1),
+(43, 6, 1, 'Prep radish & carrots', 'Chop **daikon radish** and **carrots** into small bite-sized pieces.\r\n', NULL, NULL, 1),
+(44, 6, 2, 'Prep chicken gizzard', 'Clean **chicken gizzards** by removing membranes and string and chop into bite sized pieces.\r\n', NULL, NULL, 1),
+(45, 6, 3, 'Make the soup', 'In an electric pressure cooker, combine **gizzards**, **daikon**, **carrots**, and **stock** and cook under high pressure for **30 minutes**. Let pressure release naturally.\r\n', NULL, NULL, 1),
+(46, 6, 4, 'Mix stir fry sauce', 'Combine **gochujang**, **soy sauce**, **mirin**, and **sugar** in a bowl and set aside\r\n', NULL, NULL, 1),
+(47, 6, 5, 'Prep bok choy', 'Wash **bok choy** and cut into bite sized pieces', NULL, NULL, 1),
+(48, 6, 6, 'Prepare garlic & ginger', 'Mince **garlic** and **ginger** and set aside\r\n', NULL, NULL, 1),
+(49, 6, 7, 'Finish soup', 'Remove part or all of the gizzards from the finished soup. Mix **miso paste** into soup. Keep warm.\r\n', NULL, NULL, 1),
+(50, 6, 8, 'Stir fry gizzards and bok choy', 'In a pan set at high heat, heat **oil** until smoking, add **gizzards** and cook until browned.', NULL, NULL, 1),
+(51, 6, 8, '', 'Set gizzards aside in pan and add **minced garlic and ginger** and cook until aromatic', NULL, NULL, 2),
+(52, 6, 8, '', 'Add **bok choy** and cook until leaves and stems are wilted', NULL, NULL, 3),
+(53, 6, 8, '', 'Add **stir fry sauce** and cook for another **30 seconds**', NULL, NULL, 4),
+(54, 5, 1, 'Prep and cook rice and lentils', 'Combine and wash **lentils** and **rice** until rinsing liquid comes out clear. Combine mixture with **water** and **salt** and add to rice cooker or pressure cooker. Cook in electric pressure cooker for 15 minutes on high pressure or normal setting in a rice cooker. Natural release if using pressure cooker.\r\n\r\n', NULL, NULL, 1),
+(55, 5, 2, 'Chop onions and aromatics', 'For the *caramelized onions*, peel and dice one of the **onions**.', NULL, NULL, 1),
+(56, 5, 2, '', 'For the *crispy onions*, peel and cut the other onion into thin slices.', NULL, NULL, 2),
+(57, 5, 3, 'Steam and cook onions', 'Place **sliced onions** and **diced onions** in separate pans. Add **olive oil** in each pan and season with **salt**. Add **water** and cover with a lid. Steam each at high heat for **7-10 minutes**.', 'Two pans are active', NULL, 1),
+(58, 5, 3, '', 'For *caramelized onions*, once water evaporates from the pan remove the lid, lower heat to medium and add a small amount of water and deglaze the pan with a wooden spoon. Repeat the process until onions reach desired sweetness.', NULL, NULL, 2),
+(59, 5, 3, '', 'For *crispy onions*, once the water evaporates from the pan remove the lid, lower heat to medium-low, and separate each slice of onion from each other. Cook until desired crispiness and then remove onions from the heat.', NULL, NULL, 3),
+(60, 5, 4, 'Plate the dish', 'When the caramelized onion is cooked to the desired sweetness, add **cumin seeds** (and **sliced scallions** if using) to pan and cook for another 30 seconds. Mix **caramelized onions** and cumin to **rice and lentils**. Top the dish with **crispy onions** and the **onion-infused oil**.', NULL, NULL, 1),
+(61, 5, 2, '', 'Slice **scallions** if using', NULL, NULL, 3);
 
 -- --------------------------------------------------------
 
@@ -301,11 +376,12 @@ CREATE TABLE `recipes` (
 --
 
 INSERT INTO `recipes` (`id`, `slug`, `category`, `title`, `intro`, `reference`, `published`) VALUES
-(1, 'pasta', '', 'Roasted Tomato and Garlic Pasta', 'My spin on a recipe inspired by Kenji Lopez-Alt\'s quick fresh tomato pasta. Roasting the tomatoes in the oven adds additional flavor with less mess.', 'https://www.seriouseats.com/fast-easy-pasta-blistered-cherry-tomato-sauce-recipe', 1),
-(2, 'lettuce', '', 'Boiled Lettuce with Garlic and Oyster Sauce', 'Based on Lucas Sin\'s recipe of the same name, I added can of tuna in order to make the dish a more complete meal and to help thicken the sauce. Serve with white rice.', 'https://food52.com/recipes/90179-chinese-style-lettuce-with-garlic-and-oyster-sauce', 1),
-(3, 'weekday-zongzi', 'rice-pulses', 'Weekday Zongzi', 'This is a reimagined version of a Cantonese zongzi. This version highlights the benefits of eating pulses like mung beans to reach macronutrient goals.', 'https://www.madewithlau.com/recipes/bamboo-sticky-rice', 1),
-(4, 'bean-sweet-potato', 'rice-pulses', 'Sweet Potato and Bean Bowl', 'Combining two long cooking ingredients to form a base of a high protein and highly customizable meal.', NULL, 1),
-(5, 'weekday-mujadara', '', 'Weekday Mujadara', 'A weekday version of Middle Eastern lentil that is ready as soon as the rice and lentils are done cooking, though it does require attention of two separate pans at the stove.\r\n', 'https://feelgoodfoodie.net/recipe/mujadara/\r\n', 0);
+(1, 'pasta', '', 'Roasted Tomato & Garlic Pasta', 'My spin on a recipe inspired by Kenji Lopez-Alt\'s quick fresh tomato pasta. Roasting the tomatoes in the oven adds additional flavor with less mess.', 'https://www.seriouseats.com/fast-easy-pasta-blistered-cherry-tomato-sauce-recipe', 1),
+(2, 'lettuce', '', 'Boiled Lettuce with Garlic & Oyster Sauce', 'Based on Lucas Sin\'s recipe of the same name, I added can of tuna in order to make the dish a more complete meal and to help thicken the sauce. Serve with white rice.', 'https://food52.com/recipes/90179-chinese-style-lettuce-with-garlic-and-oyster-sauce', 1),
+(3, 'weekday-rice-mungbean', 'Rice and Pulses', 'Weekday Rice & Mung Bean', 'This is a reimagined version of a Cantonese zongzi. This version highlights the benefits of eating pulses like mung beans to reach macronutrient goals.', 'https://www.madewithlau.com/recipes/bamboo-sticky-rice', 1),
+(4, 'bean-sweet-potato', 'Rice and Pulses', 'Sweet Potato & Pinto Bean Bowl', 'Combining two long cooking ingredients to form a base of a high protein and highly customizable meal.', NULL, 1),
+(5, 'weekday-rice-lentils', 'Rice and Pulses', 'Weekday Rice & Lentils', 'A dish inspired by the Middle Eastern dish mujadara that is ready as soon as the rice and lentils are done cooking, though it does require attention of two separate pans at the stove.\n', 'https://feelgoodfoodie.net/recipe/mujadara/\r\n', 0),
+(6, 'gizzard-soup-stirfry', '', 'Chicken Gizzard Soup & Stir Fry', 'Cooking chicken gizzards in a pressure cooker to help form a base for a Japanese soup and then serving the tender gizzards in a stir-fry sauce based on Japanese grilled offal. Serve with white rice.', NULL, 0);
 
 -- --------------------------------------------------------
 
@@ -348,7 +424,19 @@ INSERT INTO `steps` (`id`, `recipe_id`, `step`) VALUES
 (23, 3, 2),
 (24, 3, 3),
 (25, 3, 4),
-(26, 3, 5);
+(26, 3, 5),
+(27, 6, 1),
+(28, 6, 2),
+(29, 6, 3),
+(30, 6, 4),
+(31, 6, 5),
+(32, 6, 6),
+(33, 6, 7),
+(34, 6, 8),
+(35, 5, 1),
+(36, 5, 2),
+(37, 5, 3),
+(38, 5, 4);
 
 --
 -- Indexes for dumped tables
@@ -405,43 +493,43 @@ ALTER TABLE `steps`
 -- AUTO_INCREMENT for table `calculator`
 --
 ALTER TABLE `calculator`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `context`
 --
 ALTER TABLE `context`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
 -- AUTO_INCREMENT for table `ingredients`
 --
 ALTER TABLE `ingredients`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=85;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=170;
 
 --
 -- AUTO_INCREMENT for table `ingredient_groups`
 --
 ALTER TABLE `ingredient_groups`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
 -- AUTO_INCREMENT for table `instructions`
 --
 ALTER TABLE `instructions`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
 
 --
 -- AUTO_INCREMENT for table `recipes`
 --
 ALTER TABLE `recipes`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `steps`
 --
 ALTER TABLE `steps`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
