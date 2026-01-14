338a9888d91cd691f38f645490917dc4b6c1f53c
Sai-Kit Hui <saikit@areyoudesign.com>
2026-01-14 17:10:19 -0500

    update config

--- playwright.config.ts
+++ playwright.config.ts
@@ -82,7 +82,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run dev',
-    url: 'http://localhost:5174',
+    url: process.env.VITE_BASE_URL,
     reuseExistingServer: !process.env.CI,
     timeout: 120 * 1000,
   },
